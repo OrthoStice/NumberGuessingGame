5281f01efcd1e3b8b70f9f6e77e78cb8065b530b
Shelley Hicklin <shicklin@aperturecvo.com>
2019-05-08 14:48:43 -0400

    Beginning brainstorm

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -15,7 +15,17 @@ namespace NumberGuessingGame
             Console.WriteLine("I'm thinking of a number between {0} and {1}.  Can you guess it in 3 tries?", min, max);
 
             // TODO: Create a console app that picks a random number and then gives the user 3 chances to guess it.
-            // Use our GenerateRandomNumber() method(aka a function) to generate the number.
+
+
+            //TODO: Use our GenerateRandomNumber() method to generate the number and store it in a variable.
+
+            //TODO: while loop for three guesses
+            //ask the user to guess a number between 1 and 20 (use console.writeline) 
+            //store the users answer in a variable
+            //use an if else conditional to determine if their guess is equal to yours 
+                    //if equal win, give them a win message
+
+
             // With each incorrect answer tell the user if the correct answer is higher or lower.
             // At the end ask if they want to play again or end.
 
