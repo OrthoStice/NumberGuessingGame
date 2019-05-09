152fa328ddb3c889e5ffb3a075c050d13243d843
Shelley Hicklin <shicklin@aperturecvo.com>
2019-05-09 10:40:35 -0400

    better program flow and comments for learning

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -10,27 +10,67 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
+            //setting tryAgain value to true so game continues until the user chooses to end
+            bool tryAgain = true;
+
+            //instantiating a variable to take a user entry
+            string entry;
+            //min, max, and numbeOfTries can be changed in one place to adjust difficulty of the game
             int min = 1;
             int max = 20;
-            Console.WriteLine("I'm thinking of a number between {0} and {1}.  Can you guess it in 3 tries?", min, max);
+            int numberOfTries = 3;
 
-            // TODO: Create a console app that picks a random number and then gives the user 3 chances to guess it.
+            //a while loop that keeps going as long as tryAgain stays true. tryAgain can be set to false to break out 
+            while (tryAgain == true)
+            {
+                //set to determine if the user won or not 
+                bool winningGuess = false;
+                //gets our random number for the user to try to guess
+                int random = GenerateRandomNumber(min, max);
+                Console.WriteLine("I'm thinking of a number between {0} and {1}. Can you guess it in {2} tries?", min, max, numberOfTries);
 
+                //asks user to enter a number for the numberOfTries we want to give them
+                for (int i = 0; i < numberOfTries ; i++)
+                {
+                    Console.WriteLine("Enter a number between {0} and {1}:", min, max);
+                    var guess = Console.ReadLine();
+                        //evaluates if the number was too high too low or just right and tells the user
+                        if (int.Parse(guess) > random)
+                        {
+                            Console.WriteLine("Your guess was too high. ");
+                        }
+                        if (int.Parse(guess) < random)
+                        {
+                            Console.Write("Your guess was too low. ");
+                        }
+                        if (int.Parse(guess) == random)
+                        {
+                            winningGuess = true;
+                            break;
+                        }
+                }
 
-            //TODO: Use our GenerateRandomNumber() method to generate the number and store it in a variable.
+                //send this message only if they didn't guess correctly
+                if (winningGuess == !true)
+                {
+                    Console.WriteLine("The number I was thinking of was {0}. You didn't get it this time :(", random);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!! The winning number was {0}.", random);
+                    Console.WriteLine();
+                }
 
-            //TODO: while loop for three guesses
-            //ask the user to guess a number between 1 and 20 (use console.writeline) 
-            //store the users answer in a variable
-            //use an if else conditional to determine if their guess is equal to yours 
-                    //if equal win, give them a win message
+                Console.WriteLine("Do you want to try again? Type Y or N", random);
+                entry = Console.ReadLine();
 
-
-            // With each incorrect answer tell the user if the correct answer is higher or lower.
-            // At the end ask if they want to play again or end.
-
-            Console.ReadKey();
-
+                //if the user selects no then we won't try again
+                if (entry.ToLower().Equals("n"))
+                {
+                    tryAgain = false;
+                }
+            } 
         }
 
         static int GenerateRandomNumber(int min, int max)
