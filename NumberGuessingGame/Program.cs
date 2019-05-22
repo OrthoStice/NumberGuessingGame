73d53ef29cf1c5a699e23c7d0bdfdc009e68b5e6
kemery <kemery@aperturecvo.com>
2019-05-22 15:03:30 -0400

    Completed working version of numbers game

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,42 +1,42 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NumberGuessingGame
+namespace KimGuessingGame
 {
     class Program
     {
+        static int GenerateRandomNumber(int min, int max)
+        {
+            Random rnd = new Random();
+            return rnd.Next(min, max);
+        }
+
         static void Main(string[] args)
         {
             int min = 1;
             int max = 20;
-            Console.WriteLine("I'm thinking of a number between {0} and {1}.  Can you guess it in 3 tries?", min, max);
+            int returnValue = GenerateRandomNumber(min, max);
+            int Guess = 0;
+            int numGuesses = 0;
 
-            // TODO: Create a console app that picks a random number and then gives the user 3 chances to guess it.
+            Console.WriteLine("I am thinking of a number between {0}-{1}.  Can you guess what it is?", min, max);
 
-
-            //TODO: Use our GenerateRandomNumber() method to generate the number and store it in a variable.
-
-            //TODO: while loop for three guesses
-            //ask the user to guess a number between 1 and 20 (use console.writeline) 
-            //store the users answer in a variable
-            //use an if else conditional to determine if their guess is equal to yours 
-                    //if equal win, give them a win message
-
-
-            // With each incorrect answer tell the user if the correct answer is higher or lower.
-            // At the end ask if they want to play again or end.
-
-            Console.ReadKey();
-
-        }
-
-        static int GenerateRandomNumber(int min, int max)
-        {
-            Random rnd = new Random();
-            return rnd.Next(min,max);
+            while (Guess != returnValue)
+            {
+                Guess = Convert.ToInt32(Console.ReadLine());
+                if (Guess < returnValue)
+                {
+                    Console.WriteLine("No, the number I am thinking of is higher than " + Guess + " .  Can you guess what it is?");
+                }
+                if (Guess > returnValue)
+                {
+                    Console.WriteLine("No, the number I am thinking of is lower than " + Guess + " .  Can you guess what it is");
+                }
+            }
+            numGuesses++;
+            Console.WriteLine("Well done! The answer was " + returnValue + ".\nYou took " + numGuesses + " guesses.");
         }
     }
+
 }
+
+
